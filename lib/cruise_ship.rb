49405904b9aab95bd19bba63ec862231d0c181dc
Richard Fenix <richard.fenix@gmail.com>
2018-05-31 22:04:40 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,21 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
+passengers = {
+suite_a: "Amanda Presley", 
+suite_b: "Seymour Hoffman", 
+suite_c: "Alfred Tennyson", 
+suite_d: "Charlie Chaplin", 
+suite_e: "Crumpet the Elf"
+}
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each {|room, guest|
+    if room == :suite_a && guest.start_with?("A")
+      winner = guest
+    end
+  }
+  winner
+  
 end
+
+puts select_winner(passengers)
