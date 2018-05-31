d4e6050922724b365013bf19448e6a383932ac09
Richard Fenix <richard.fenix@gmail.com>
2018-05-31 22:03:26 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,7 +9,7 @@ suite_e: "Crumpet the Elf"
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each {|room, guest|
-    if room == :suite_a && guest.starts_with?("A")
+    if room == :suite_a && guest.start_with?("A")
       winner = guest
     end
   }
